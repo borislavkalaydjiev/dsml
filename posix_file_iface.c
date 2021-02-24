fe6bea65238160e0a1898e61ca9d77eefa11b9ee
borislavkalaydjiev <borislav.kalaydjiev@gmail.com>
2021-02-24 18:37:29 +0200

    Missing definitions

--- posix_file_iface.c
+++ posix_file_iface.c
@@ -6,6 +6,17 @@
 #include <dsml/score/debug.h>
 #include <dsml/score/posix_file_iface.h>
 
+#ifndef __unix__
+#error "Compiling on a none UNIX platform!"
+#else /* #ifndef __unix__ */
+#include <unistd.h>
+#include <sys/stat.h> /* NOTE: posix optional include! */
+#include <fcntl.h>
+#include <errno.h>
+#if !defined(_POSIX_VERSION) || !defined(_POSIX2_VERSION)
+#error "Compiling on a none POSIX UNIX platform!"
+#endif /* #if !defined(_POSIX_VERSION) || !defined(_POSIX2_VERSION) */
+#endif /* #ifndef __unix__ */
 
 DSML_STATIC int32 dsml_posix_file_open(DSML_CONTEXT_ARG, struct dsml_file_tag * const f, uint16 oflag)
 {
