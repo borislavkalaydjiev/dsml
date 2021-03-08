ca74557f5c67ff7c9e8d7146eab67dd0a013696f
borislavkalaydjiev <borislav.kalaydjiev@gmail.com>
2021-03-08 10:16:28 +0200

    Extended with u64

--- tools.c
+++ tools.c
@@ -108,12 +108,43 @@ void dsml_tools_tx_uint32(void * const dest, uint8 ** src)
 #ifndef HOST_IS_XC167
 void dsml_tools_rx_uint64(void * const dest, uint8 ** src)
 {
-	// TODO
+	register uint8 * s;
+	DSML_FUNCTION_BEGIN();
+	DSML_ASSERT_POINTER_CHECK(dest);
+	DSML_ASSERT_POINTER_CHECK(src);
+	DSML_ASSERT_POINTER_CHECK(src[0]);
+	s = src[0];
+	*((uint64*)dest) = UINT64_C(0)
+		| ((uint64)s[0]) << 56
+		| ((uint64)s[1]) << 48
+		| ((uint64)s[2]) << 40
+		| ((uint64)s[3]) << 32
+		| ((uint64)s[4]) << 24
+		| ((uint64)s[5]) << 16
+		| ((uint64)s[6]) << 8
+		| ((uint64)s[7]) << 0;
+	*src += sizeof(uint64);
+	DSML_FUNCTION_END();
 }
 
 void dsml_tools_tx_uint64(void * const dest, uint8 ** src)
 {
-	// TODO
+	register uint8 * s;
+	DSML_FUNCTION_BEGIN();
+	DSML_ASSERT_POINTER_CHECK(dest);
+	DSML_ASSERT_POINTER_CHECK(src);
+	DSML_ASSERT_POINTER_CHECK(src[0]);
+	s = src[0];
+	*s++ = (*((uint64*)dest)) >> 56;
+	*s++ = (*((uint64*)dest)) >> 48;
+	*s++ = (*((uint64*)dest)) >> 40;
+	*s++ = (*((uint64*)dest)) >> 32;
+	*s++ = (*((uint64*)dest)) >> 24;
+	*s++ = (*((uint64*)dest)) >> 16;
+	*s++ = (*((uint64*)dest)) >> 8;
+	*s = (*((uint64*)dest)) >> 0;
+	*src += sizeof(uint64);
+	DSML_FUNCTION_END();
 }
 #endif /* #ifndef HOST_IS_XC167 */
 
